1d809aeb38dd93b5f6ca9a5ec258c2d7500cb055
jgmakin <jgmakin@gmail.com>
2024-10-15 21:25:02 -0400

    common_texification

--- src/tikzplotlib/_axes.py
+++ src/tikzplotlib/_axes.py
@@ -1,7 +1,8 @@
 import matplotlib as mpl
 import numpy as np
 from matplotlib.backends.backend_pgf import (
-    common_texification as mpl_common_texification,
+    _tex_escape as mpl_common_texification,
+    # common_texification as mpl_common_texification,
 )
 
 from . import _color
